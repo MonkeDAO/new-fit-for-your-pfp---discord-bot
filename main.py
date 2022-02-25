33a51c66591cb9ce110a1060129f9b639e3906f3
Britt Preyer <brittpreyer@britts-air.lan>
2022-02-25 17:05:51 -0500

    added vote button

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 import requests
 import PIL
 from PIL import Image
+from dotenv import load_dotenv
 
 # discord bot
 bot = commands.Bot(command_prefix="!")
@@ -25,11 +26,12 @@ pfp_atts = json.load(data)
 
 # list of the various outfits you want to offer. these should match the filename on the outfit pngs
 
-outfits = ["suit", "blue"]
+outfits = ["suit", "blue", "vote"]
 
 # Search for the pfp id in the JSON dictionary and return the image URL associated with that id. You'll need to update the keys to match what's in your JSON delattr
 
 # Need to add error handling
+
 
 def get_pfp_img_url(id):
     for pfp in pfp_atts:
@@ -49,11 +51,12 @@ def download_image(url, image_file_path):
 
 # Combines the pfp image with a transparent png of the attribute  and saves it to an output directory
 
+
 def get_dressed(fit, pfp_id):
     url = (get_pfp_img_url(pfp_id))
     download_image(url, pfp_folder + str(pfp_id) + '.png')
 
-# This combines the images 
+# This combines the images
 
     pfp = Image.open(pfp_folder + str(pfp_id) + '.png')
     outfit = Image.open(outfits_folder + fit + '.png')
@@ -63,33 +66,42 @@ def get_dressed(fit, pfp_id):
 
     return
 
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
 # !newfit command executes the get_dressed function and returns the resulting image. It accepts a value between 1 and 5000. Update this to use the command name you want and the values to fit the range of your project
 
+
 @bot.command(name="newfit", brief='Dress your pfp', description='This command will let you apply new fits to your pfp')
 async def newfit(ctx, fit: str, pfp_id: int):
     try:
-      if fit.lower() in outfits:
-        if 0 <= pfp_id <= 5000:
-            get_dressed(fit, str(pfp_id))
-            await ctx.channel.send(file=discord.File(save_img_folder + 'dressed' + str(pfp_id) +'.png'))
-      else: 
-        await ctx.send('Please enter a valid fit. Check !fits for options')
+        if fit.lower() in outfits:
+            if 0 <= pfp_id <= 5000:
+                get_dressed(fit, str(pfp_id))
+                await ctx.channel.send(file=discord.File(save_img_folder + 'dressed' + str(pfp_id) + '.png'))
+        else:
+            await ctx.send('Please enter a valid fit. Check !fits for options')
     except:
         await ctx.send('Please enter a valid number between 1 and 5000.')
 
-# Lists the different "fits" available. This just returns the outfits list on new lines 
+# Lists the different "fits" available. This just returns the outfits list on new lines
+
 
 @bot.command(brief='List avaiable fits', description='This command will list the different outfits available to you')
 async def fits(ctx):
-    await ctx.send('**List of Fits (please choose from one of the below)**\n\n'+ "\n".join(outfits))
+    await ctx.send('**List of Fits (please choose from one of the below)**\n\n' + "\n".join(outfits))
 
 # Lets user know when they enter an invalid command
+
+
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound): # or discord.ext.commands.errors.CommandNotFound as you wrote
+    # or discord.ext.commands.errors.CommandNotFound as you wrote
+    if isinstance(error, commands.CommandNotFound):
         await ctx.send("Unknown command, please check !help for a list of available commands")
+
+load_dotenv()
+
 bot.run(os.getenv('TOKEN'))
